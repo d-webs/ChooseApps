fce1f55cfd0062629d21b3d3540547b34691674d
David Webster <dwebster@appacademy.io>
2018-02-09 10:11:06 -0800

    Add package.json and webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/choose_apps.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react', 'stage-2']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
