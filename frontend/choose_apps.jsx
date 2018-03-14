50d79c4209cfe3280b4e19b9d00a61bab0bc12a7
David Webster <dwebster@appacademy.io>
2018-03-14 00:53:06 -0700

    Begin to build out client side files with React

---
+++ frontend/choose_apps.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+document.addEventListener('DOMContentLoaded',  () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>Hello from React!</h1>, rootEl);
+});
